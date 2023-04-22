f7b68839fd0e7e6ad4dd31ac3c54e45ca5b91599
Son <thsgygus23@naver.com>
2023-04-22 20:22:52 +0900

    library service commit

---
+++ src/main/java/com/korit/library/web/api/LikeApi.java
@@ -0,0 +1,37 @@
+package com.korit.library.web.api;
+
+import com.korit.library.security.PrincipalDetails;
+import com.korit.library.service.LikeService;
+import com.korit.library.web.dto.CMRespDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class LikeApi {
+
+    private final LikeService likeService;
+
+    @PostMapping("/book/{bookId}/like")
+    public ResponseEntity<CMRespDto<Integer>> like(@PathVariable int bookId,
+                                                   @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        int likeCount = likeService.like(bookId, principalDetails.getUser().getUserId());
+
+        return ResponseEntity.ok().body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", likeCount));
+    }
+
+    @DeleteMapping("/book/{bookId}/like")
+    public ResponseEntity<CMRespDto<Integer>> dislike(@PathVariable int bookId,
+                                                   @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        int likeCount = likeService.dislike(bookId, principalDetails.getUser().getUserId());
+
+        return ResponseEntity.ok().body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", likeCount));
+    }
+
+}
